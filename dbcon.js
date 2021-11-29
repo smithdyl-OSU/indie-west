73a9bfb9a230f0e3479b2f5cf13bb7537e7483d3
alexsdeatherage <alexdeatherage4829@gmail.com>
2021-11-28 16:26:21 -0800

    add in changes to dbcon and index" \

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,14 @@
+// get instance of mysql connection
 const mysql = require('mysql')
+
+// create a connection pool using the crednetials below
 const pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_deatheal',
-    password        : '0274',
-    database        : 'cs340_deatheal'
+    host            : 'us-cdbr-east-04.cleardb.com',
+    user            : 'bc4d0d06feb538',
+    password        : '0cdf823c',
+    database        : 'heroku_0863c25103b153e'
 });
 
+// export the pool for our application to use
 module.exports.pool = pool;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-
+const handlebars = require('express-handlebars');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -10,3 +10,64 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
+
+// Variable Database
+const db = require('./db.js');
+
+
+// Routes
+
+// Renders the home page
+app,get('/', (req, res) => {
+    res.render('home')
+    });
+
+// 
+
+// Artists //
+
+// Get/Show all artists
+app.get('/artists', (req, res) => {
+    let query = 'SELECT * FROM artists';
+
+    db.pool.query(query, (err, rows) => {
+        res.render('artists', { data: rows });
+    });
+});
+
+// Filters artists by name
+app.get('/artists/:name', (req, res) => {
+    let query = 'SELECT * FROM artists WHERE name = ?';
+    let name = req.params.name;
+
+    db.pool.query(query, [name], (err, rows) => {
+        res.render('artists', { data: rows });
+    });
+});
+
+
+// Add a new artist to the database
+app.post('/artists', (req, res) => {
+    
+
+
+
+
+
+// Renders a 404 status code if the page is not found
+app.use(function(req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+
+// Renders a 500 status code if there is an error
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+// App starts listening on giben port
+app.listen(app.get('port'), () => {
+    console.log('Server started on port ' + app.get('port') + '; press Ctrl-C to terminate.');
+});
