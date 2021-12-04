30059398e6d5782061fe27d17db027de7720c6d3
Dylan <smithdyl@oregonstate.edu>
2021-12-04 12:03:08 -0800

    Update artists.js
    Revert changes

--- artists.js
+++ artists.js
@@ -29,42 +29,5 @@ module.exports = function () {
         }
     });
 
-    // insert new artist to database
-    router.post('/', [
-        // validations from express-validator
-        body(['name']).trim()
-            .isAscii().withMessage('must be at least one character (ASCII characters only)'),
-    ], (req, res) => {
-        const errors = validationResult(req);
-
-        const data = {
-            page_title: 'Add Artist',
-        };
-
-        if (!errors.isEmpty()) {
-            errors.array().forEach(error => {
-                req.flash('danger', `${error.param} error: ${error.msg}`);
-            });
-
-            res.render('artists/new', data);
-        } else {
-            // parameters for insert query
-            const newArtist = {
-                name: req.body.name
-            };
-
-            Artist.create(newArtist, (err, artist) => {
-                if (err) {
-                    req.flash('danger', err['msg'] || err['sqlMessage']);
-                }
-                else {
-                    req.flash('success', `Artist created: ${artist.name}!`);
-                }
-
-                res.redirect('/artists/');
-            });
-        }
-    });
-
     return router;
 }();
