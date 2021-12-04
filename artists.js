8a782795e628f77ad0054096ae5e31190ec6605b
Dylan <smithdyl@oregonstate.edu>
2021-12-04 11:59:53 -0800

    Update artists.js
    Testing INSERT for artists

--- artists.js
+++ artists.js
@@ -1,10 +1,10 @@
-module.exports = function() {
+module.exports = function () {
     const express = require('express');
     const router = express.Router();
 
     // function that gets artist information from the database
     function getArtists(res, mysql, context, complete) {
-        mysql.pool.query("SELECT artistID, name FROM artists", function(error, results, fields) {
+        mysql.pool.query("SELECT artistID, name FROM artists", function (error, results, fields) {
             if (error) {
                 res.write(JSON.stringify(error));
                 res.end();
@@ -29,7 +29,42 @@ module.exports = function() {
         }
     });
 
+    // insert new artist to database
+    router.post('/', [
+        // validations from express-validator
+        body(['name']).trim()
+            .isAscii().withMessage('must be at least one character (ASCII characters only)'),
+    ], (req, res) => {
+        const errors = validationResult(req);
 
+        const data = {
+            page_title: 'Add Artist',
+        };
+
+        if (!errors.isEmpty()) {
+            errors.array().forEach(error => {
+                req.flash('danger', `${error.param} error: ${error.msg}`);
+            });
+
+            res.render('artists/new', data);
+        } else {
+            // parameters for insert query
+            const newArtist = {
+                name: req.body.name
+            };
+
+            Artist.create(newArtist, (err, artist) => {
+                if (err) {
+                    req.flash('danger', err['msg'] || err['sqlMessage']);
+                }
+                else {
+                    req.flash('success', `Artist created: ${artist.name}!`);
+                }
+
+                res.redirect('/artists/');
+            });
+        }
+    });
 
     return router;
 }();
