c3a2de35f8b9300a424dbfe352386b6a7fe8e91f
Dylan <smithdyl@oregonstate.edu>
2021-12-05 20:48:56 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,11 +3,11 @@ const mysql = require('mysql')
 
 // create a connection pool using the crednetials below
 const pool = mysql.createPool({
-    connectionLimit : 10,
-    host            : 'us-cdbr-east-04.cleardb.com',
-    user            : 'bc4d0d06feb538',
-    password        : '0cdf823c',
-    database        : 'heroku_0863c25103b153e'
+    connectionLimit: 100,
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'bc4d0d06feb538',
+    password: '0cdf823c',
+    database: 'heroku_0863c25103b153e'
 });
 
 // export the pool for our application to use
