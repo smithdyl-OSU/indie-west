d471f23cf92196dfa0699de16408ac4a64e11521
Alex Deatherage <alexdeatherage4829@gmail.com>
2021-12-06 20:24:56 -0800

    work on UPDATE for artists

--- artists.js
+++ artists.js
@@ -14,11 +14,26 @@ module.exports = function () {
         });
     }
 
+    // function that gets one artist for updating
+    function getArtistToUpdate(res, mysql, context, id, complete) {
+        let sql = "SELECT id as artistID, name FROM artists WHERE artistID = ?";
+        let inserts = [id];
+        mysql.pool.query(sql, inserts, function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.artists = results[0];
+            complete();
+        });
+    }
+
+
     // displays artists page
     router.get('/', (req, res) => {
         let callbackCount = 0;
         let context = {}; // context object to pass to the callback function
-        context.jsscripts = ['deleteArtist.js']; // array of javascript files to include in the page
+        context.jsscripts = ['deleteArtist.js', 'searchArtist.js']; // array of javascript files to include in the page
         let mysql = req.app.get('mysql');
         getArtists(res, mysql, context, complete);
         function complete() {
@@ -28,6 +43,22 @@ module.exports = function () {
             }
         }
     });
+    // gets the updateArtists page using the artistID
+    router.get('/:id', (req, res) => {
+        let callbackCount = 0;
+        let context = {}; // context object to pass to the callback function
+        context.jsscripts = ['updateArtist.js']; // array of javascript files to include in the page
+        let mysql = req.app.get('mysql');
+        getArtistToUpdate(res, mysql, context, req.params.artistID, complete);
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('updateArtists', context);
+            }
+        }
+    });
+
+
 
     // insert new artist to database
     router.post('/', function (req, res) {
@@ -46,6 +77,25 @@ module.exports = function () {
             }
         });
     });
+
+    // updates artist information in database
+    router.put('/:id', function (req, res) {
+        let mysql = req.app.get('mysql');
+        console.log(req.body)
+        let sql = "UPDATE artists SET name=? WHERE artistID=?";
+        let values = [req.body.name, req.params.id];
+        sql = mysql.pool.query(sql, values, function (error, results, fields) {
+            if (error) {
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.end();
+            } else {
+                res.status(200);
+                res.end();
+            }
+        });
+    });
+
 
     // delete artist from database
     router.delete('/:id', function (req, res) {
